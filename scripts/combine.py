67a23b4de11ed15c8e2a834bede09425acd7e93f
venerari <venerayan@gmail.com>
2018-06-05 16:09:44 -0400

    3

--- scripts/combine.py
+++ scripts/combine.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
 
-with open('./windows.csv') as f1, open('./comma.csv') as f2:
+with open('./wins.csv') as f1, open('./comma.csv') as f2:
     print '\n'.join((a.rstrip() + b.rstrip() for a, b in zip(f1, f2)))
 
